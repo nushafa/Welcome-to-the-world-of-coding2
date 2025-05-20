14b245071f4af00fca2270c1cff75e969a3530fd
Abiat Nushafa <nushafa1@gmail.com>
2025-05-20 20:50:50 +0600

    Add files via upload

---
+++ Welcome to the world of coding.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("Welcome to the world of programming!")
+
+#print a number
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#pass multiple arguments for printing
+print("Hello" ,5)
+
+#End argument of Print Statement
+print("welcome to ", end= "*")
